ff2c74d502a58b774110b96d5de7b2f19326d23a
缪佳炜 <mjw19961218@hotmail.com>
2025-03-14 09:39:13 +0800

    部分优化

--- data_processor.py
+++ data_processor.py
@@ -22,16 +22,14 @@ def lorentzian(x: np.ndarray, amplitude: float, center: float, sigma: float) -> 
     return amplitude / (1 + ((x - center) / sigma)**2)
 
 # 定义 Gaussian 函数
-def lorentzian(x: np.ndarray, amplitude: float, center: float, sigma: float) -> np.ndarray:
+def gaussian(x: np.ndarray, amplitude: float, center: float, sigma: float) -> np.ndarray:
     # Gaussian 函数
     return amplitude * np.exp(-((x - center) / sigma)**2)
 
 # 定义 Pseudo-Voigt 函数
 def pseudo_voigt(x: np.ndarray, amplitude: float, center: float, sigma: float, eta: float) -> np.ndarray:
     # Pseudo Voigt 函数：高斯函数和洛伦兹函数的线性组合
-    gaussian = amplitude * np.exp(-((x - center) / sigma)**2)
-    lorentzian = amplitude / (1 + ((x - center) / sigma)**2)
-    return eta * lorentzian + (1 - eta) * gaussian
+    return eta * lorentzian(x, amplitude, center, sigma) + (1 - eta) * gaussian(x, amplitude, center, sigma)
 
 # 定义二阶切比雪夫多项式
 def chebyshev(x: np.ndarray, c0, c1, c2):
@@ -47,6 +45,11 @@ def single_peak(x: np.ndarray, a1, x01, w_L1, w_R1, m1, c0, c1, c2):
 def double_peak(x, a1, x01, w_L1, w_R1, m1, a2, x02, w_L2, w_R2, m2, c0, c1, c2):
     # 两个 Split-Pearson VII 峰 + 二阶切比雪夫背景
     return (split_pearson_vii(x, a1, x01, w_L1, w_R1, m1) + split_pearson_vii(x, a2, x02, w_L2, w_R2, m2) + chebyshev(x, c0, c1, c2))
+
+# 定义多峰拟合函数（Split-Pearson VII 峰 + 二阶切比雪夫背景）
+def oi_peak(x, a1, x01, w_L1, w_R1, m1, a2, x02, w_L2, w_R2, m2, a3, x03, w_L3, w_R3, m3, a4, x04, w_L4, w_R4, m4, a5, x05, w_L5, w_R5, m5, c0, c1, c2):
+    # 5个 Split-Pearson VII 峰 + 二阶切比雪夫背景
+    return (split_pearson_vii(x, a1, x01, w_L1, w_R1, m1) + split_pearson_vii(x, a2, x02, w_L2, w_R2, m2) + split_pearson_vii(x, a3, x03, w_L3, w_R3, m3) + split_pearson_vii(x, a4, x04, w_L4, w_R4, m4) + split_pearson_vii(x, a5, x05, w_L5, w_R5, m5) + chebyshev(x, c0, c1, c2))
 
 # 计算 Split-Pearson VII 函数的半峰宽
 def calculate_fwhm_spv(w_L, w_R, m):
@@ -123,7 +126,7 @@ def fit_data_sifwhm(x, y):
         return popt, y
     except RuntimeError as e:
         print(f"拟合失败: {e}")
-        return None, None
+        return None, y
 
 # Si[111] 单峰曲线计算
 def fit_peak_sifwhm(x, popt):
@@ -132,12 +135,8 @@ def fit_peak_sifwhm(x, popt):
     silicon_peak = split_pearson_vii(x, popt[0], popt[1], popt[2], popt[3], popt[4])
     return fitted_curve, background, silicon_peak
 
-# OI值 多峰曲线计算
+# OI值 多峰曲线拟合数据
 def fit_data_oi(x, y):
-    # 定义多峰拟合函数（Split-Pearson VII 峰 + 二阶切比雪夫背景）
-    def oi_peak(x, a1, x01, w_L1, w_R1, m1, a2, x02, w_L2, w_R2, m2, a3, x03, w_L3, w_R3, m3, a4, x04, w_L4, w_R4, m4, a5, x05, w_L5, w_R5, m5, c0, c1, c2):
-        # 5个 Split-Pearson VII 峰 + 二阶切比雪夫背景
-        return (split_pearson_vii(x, a1, x01, w_L1, w_R1, m1) + split_pearson_vii(x, a2, x02, w_L2, w_R2, m2) + split_pearson_vii(x, a3, x03, w_L3, w_R3, m3) + split_pearson_vii(x, a4, x04, w_L4, w_R4, m4) + split_pearson_vii(x, a5, x05, w_L5, w_R5, m5) + chebyshev(x, c0, c1, c2))
     p0 = [max(y), 54.23, 0.1, 0.1, 1.5, max(y), 56.12, 0.1, 0.1, 1.5, max(y), 69.14, 0.1, 0.1, 1.5, max(y), 76.38, 0.1, 0.1, 1.5, max(y), 77.55, 0.1, 0.1, 1.5, 0, 0, 0]
     try:
         popt, _ = curve_fit(oi_peak, x, y, p0=p0)
@@ -145,3 +144,15 @@ def fit_data_oi(x, y):
     except RuntimeError as e:
         print(f"拟合失败: {e}")
         return None
+    
+# OI值 多峰曲线计算
+def fit_peak_oi(x, popt):
+    fitted_curve = oi_peak(x, *popt)
+    g004_peak = split_pearson_vii(x, popt[0], popt[1], popt[2], popt[3], popt[4])
+    si311_peak = split_pearson_vii(x, popt[5], popt[6], popt[7], popt[8], popt[9])
+    si400_peak = split_pearson_vii(x, popt[10], popt[11], popt[12], popt[13], popt[14])
+    si331_peak = split_pearson_vii(x, popt[15], popt[16], popt[17], popt[18], popt[19])
+    g110_peak = split_pearson_vii(x, popt[20], popt[21], popt[22], popt[23], popt[24])
+    background = chebyshev(x, popt[25], popt[26] , popt[27])
+
+    return fitted_curve, background, g004_peak, si311_peak, si400_peak, si331_peak, g110_peak
